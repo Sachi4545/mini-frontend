71824f437ad662d6e7dced702cadef50f765fc8f
Sachita <sachita2825@gmail.com>
2026-05-20 12:03:55 +0530

    removed unused api variable

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-const API = "https://mini-n54f.onrender.com";
+
 
 function Dashboard() {
 
